4927ba2cd4436e48caaa8daad12243160d0aee27
Borna Maticic <bornamaticic@yahoo.com>
2018-10-27 19:32:07 +0100

    Finished MVP and half of extension.

--- room.rb
+++ room.rb
@@ -1,11 +1,12 @@
 class Room
 
-  attr_reader :name, :entry_fee, :capacity
+  attr_reader :name, :entry_fee, :capacity, :songs
 
-  def initialize(name, entry_fee, capacity)
+  def initialize(name, entry_fee, capacity, songs)
     @name = name
     @entry_fee = entry_fee
     @capacity = capacity
+    @songs = songs
   end
 
   def room_capacity
@@ -15,6 +16,17 @@ class Room
 
   def room_entry_fee
     @entry_fee
+  end
+
+  def check_in_guest_in_room
+    room = [@room1]
+    room << @guest1
+    return "Borna has joined Room1."
+  end
+
+  def check_out_guest_from_room
+    check_in_guest_in_room.clear
+    return "Borna has left Room1."
   end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -2,22 +2,23 @@ require( 'minitest/autorun' )
 require( 'minitest/rg' )
 require_relative( '../guest' )
 require_relative( '../room' )
+require_relative( '../song' )
 
 
 
 class TestRoom < MiniTest::Test
 
   def setup
-    @room1 = Room.new("Autumn", 10, 10)
-    @room2 = Room.new("Summer", 20, 15)
-    @room3 = Room.new("Spring", 30, 20)
+    @room1 = Room.new("Autumn", 10, 10, "Smells like teen spirit")
+    @room2 = Room.new("Summer", 20, 15, "Hit me baby one  more time")
 
-    # @song1 = Song.new("Smells like teen spirit")
-    # @song2 = Song.new("Hit me baby one  more time")
-    # playlist = [@song1, @song2]
-    #
-    # @guest1 = Guest.new("Borna", 40)
-    # @guest2 = Guest.new("Aiste", 20)
+
+    @song1 = Song.new("Smells like teen spirit")
+    @song2 = Song.new("Hit me baby one  more time")
+    songs = [@song1, @song2]
+
+    @guest1 = Guest.new("Borna", 20)
+    @guest2 = Guest.new("Aiste", 10)
 
   end
 
@@ -27,12 +28,29 @@ class TestRoom < MiniTest::Test
 
 
   def test_room_capacity
-    assert_equal(20, @room3.room_capacity)
+    assert_equal(15, @room2.room_capacity)
   end
 
   def test_room_entry_fee
     assert_equal(10, @room1.room_entry_fee)
   end
 
+  def test_room_has_song
+    expected = "Smells like teen spirit"
+    actual = @room1.songs
+    assert_equal(expected, actual)
+  end
 
+  def test_check_in_guest_in_room
+    expected = "Borna has joined Room1."
+    actual = @room1.check_in_guest_in_room
+    assert_equal(expected, actual)
+  end
+
+  def test_check_out_guest_from_room
+    expected = "Borna has left Room1."
+    actual = @room1.check_out_guest_from_room
+    assert_equal(expected,actual)
+  end
+  
 end
